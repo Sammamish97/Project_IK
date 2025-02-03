6d878299b81a3e60c53a674dbad987356ab575d7
Sinil <rtd99062@gmail.com>
2025-02-03 23:33:52 +0900

    #111 - Fix minor bug, #113 - Implement necessary structs for perks.
    PerkProgress, IK_Perks_Progress - A data to record how the tree grow up.
    TimeDilationManager - Fix minor bug. Slow time is not applied when the current dilation is 1.f.
    PerkTree - A perk tree that has a lot of node. It is single-ton class.
    PerkNode - A node of tree.

---
+++ Source/Project_IK/Private/Structs/PerkTree.cpp
@@ -0,0 +1,28 @@
+/******************************************************************************
+Copyright(C) 2024
+Author: sinil.kang(rtd99062@gmail.com)
+Creation Date : 2.3.2025
+Summary : Source file for Perk trees.
+
+Licensed under the MIT License.
+See LICENSE file in the project root for full license information.
+******************************************************************************/
+
+
+#include "Structs/PerkTree.h"
+
+UPerkTree::UPerkTree()
+	:Super::UObject()
+{
+
+}
+
+UPerkTree* UPerkTree::Get()
+{
+	return nullptr;
+}
+
+void UPerkTree::Destroy()
+{
+
+}
---
+++ Source/Project_IK/Public/Structs/PerkTree.h
@@ -0,0 +1,41 @@
+/******************************************************************************
+Copyright(C) 2024
+Author: sinil.kang(rtd99062@gmail.com)
+Creation Date : 2.3.2025
+Summary : Header file for Perk trees.
+
+Licensed under the MIT License.
+See LICENSE file in the project root for full license information.
+******************************************************************************/
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+#include "Structs/PerkNode.h"
+#include "PerkTree.generated.h"
+
+struct FPerkNode;
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECT_IK_API UPerkTree : public UObject
+{
+	GENERATED_BODY()
+
+public:
+	static UPerkTree* Get();
+
+	// Need to call it to clean instance at the end of the world EndPlay()
+	void Destroy();
+
+
+	UPROPERTY(VisibleAnywhere, Category = "PerkData")
+	TMap<int32, FPerkNode> perks_;
+
+private:
+	// Private constructor to prevent multiple instancings.
+	UPerkTree();
+};
