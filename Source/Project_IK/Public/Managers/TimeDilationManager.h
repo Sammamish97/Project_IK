c300f0bc61a50dc5c561864bef19e96de05698ad
Mook Kim <chunmook.kim97@gmail.com>
2025-02-07 19:50:22 +0900

    Update from main branch.

---
+++ Source/Project_IK/Public/Managers/TimeDilationManager.h
@@ -0,0 +1,42 @@
+/******************************************************************************
+Copyright(C) 2024
+Author: sinil.kang(rtd99062@gmail.com)
+Creation Date : 2.2.2025
+Summary : Header file for manager to control TimeDilationManager.
+
+Licensed under the MIT License.
+See LICENSE file in the project root for full license information.
+******************************************************************************/
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+#include "TimeDilationManager.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECT_IK_API UTimeDilationManager : public UObject
+{
+	GENERATED_BODY()
+public:
+	UTimeDilationManager();
+
+	UFUNCTION()
+	void SetGlobalTimeDilation(UObject* world, float time_dilation);
+	UFUNCTION()
+	inline float GetGlobalTimeDilation(UObject* world) const;
+	
+	UFUNCTION()
+	void SlowGlobalTimeDilation(UObject* world);
+	UFUNCTION()
+	void RestoreGlobalTimeDilation(UObject* world);
+
+	static constexpr float SLOW_SPEED = 0.5f;
+
+private:
+	UPROPERTY()
+	float last_time_dilation_;
+};
