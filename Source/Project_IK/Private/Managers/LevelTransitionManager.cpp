ebb0add5b4ec1691bbd7c6f3488b375340bb039f
Sinil Kang <rtd99062@gmail.com>
2025-02-21 15:48:06 +0900

    #121 - Final bug fixes before recording demo video.
    BP_HeroInventoryWidget - Set text to Hero1 manually for sake of recording video.
    shield - Rename the image asset to fix typo from shiled to shield.
    IK_Armor_Data, IK_Passive_Skill_Data - Manually set data. If it caused conflicts when merged, ignore these changes. It is not important one.
    LevelTransitionManager - The manager forces us to start game from only the HeroSelectLevel because the manager need to initialize spawn_data_. Thus, the manager crashes the whole game when we start from the HeroSelectLevel. In order to avoid crashes, add a simple if checks. However, it is not an ultimate solution. Basically I need to refactor the LevelTransitionManager in near future.

--- Source/Project_IK/Private/Managers/LevelTransitionManager.cpp
+++ Source/Project_IK/Private/Managers/LevelTransitionManager.cpp
@@ -83,7 +83,14 @@ void ULevelTransitionManager::SaveData(UWorld* world)
 			const UCharacterStatComponent* stat_component = hero->GetCharacterStat();
 
 			// @@ TODO: Need to save proper data.
-			spawn_data_[i].character_data_ = stat_component->GetCharacterData();
+			if (spawn_data_.IsValidIndex(i))
+			{
+				spawn_data_[i].character_data_ = stat_component->GetCharacterData();
+			}
+			else
+			{
+				UE_LOG(LogTemp, Error, TEXT("SaveData has failed because spawn index of spawn_data_ is invalid."));
+			}
 		}
 	}
 }
@@ -126,7 +133,7 @@ void ULevelTransitionManager::SpawnHeroes(UWorld* world)
 		AHeroBase* hero = world->SpawnActor<AHeroBase>(spawn_data_[i].character_data_.unit_class_, spawn_position + FVector(0, (300.f * (spawn_data_.Num() - 1) / -2.f ) + (i * 300), 90), spawn_rotation);
 		hero->SpawnDefaultController();
 		hero->GetComponentByClass<USkillContainer>()->SetSkill(UMyTestSkill::StaticClass());
-		hero->GetComponentByClass<UCharacterStatComponent>()->SetCharacterData(spawn_data_[0].character_data_);
+		hero->GetComponentByClass<UCharacterStatComponent>()->SetCharacterData(spawn_data_[i].character_data_);
 		hero->Initialize();
 	}
 }
