13f46f859c8faeacb1dec929067ecc037c33cec0
Sinil Kang <rtd99062@gmail.com>
2025-02-27 17:03:13 +0900

    #123 - Implement rough enemy spawner manager.
    BP_EnemySpawner, EnemySpawner - Since our game does not need to fine-tune spawn location, BP_EnemySpawner is not needed. The big reason why we don't need them is the game move forwards without turn left or right.
    BP_EnemySpawnerManager - In order to handle spawning enemies, I created a manager and put it in IKGameModeBase(Combat mode). Programmers may modify enemy class will spawned, spawn distance between waves, spawn distance among enemies in a wave, and the number of enemy in a wave.
    BP_IKGameMode - It points to BP_EnemySpawnerManager.
    C++TestScene, CombatLevel - Adjust location of actors such as floor, and NavMeshBoundsVolume to support long straight battles.
    DPI_LaserBeam - Since game_mode->GetEnemyContainers return TArray<AEnemyBase*> instead of TArray<AActor*>, I adapted old code to follow the fule.
    Enemy_SpawnerManager - Need to call SpawnEnemies at the beginning of the game to initiate waves. Deck system may resolve simplicity issue of spawning enemies. It will be implemented in near future unless design not changed.

---
+++ Source/Project_IK/Private/Managers/EnemySpawnerManager.cpp
@@ -0,0 +1,94 @@
+/******************************************************************************
+Copyright(C) 2024
+Author: sinil.kang(rtd99062@gmail.com)
+Creation Date : 02.27.2025
+Summary : Source file for manager that controls spawning enemies.
+
+Licensed under the MIT License.
+See LICENSE file in the project root for full license information.
+******************************************************************************/
+
+
+#include "Managers/EnemySpawnerManager.h"
+
+#include "Characters/EnemyBase.h"
+
+
+UEnemySpawnerManager::UEnemySpawnerManager()
+	:spawn_distance_(), enemy_waves_(0), spawn_position_(), enemy_spacing_(300), enemy_num_(1)
+{
+}
+
+void UEnemySpawnerManager::Initialize(FVector base_spawn_position, int32 waves)
+{
+	spawn_position_ = base_spawn_position;
+	enemy_waves_ = waves;
+}
+
+void UEnemySpawnerManager::SpawnEnemies()
+{
+	if (enemy_waves_ <= 0)
+	{
+		return;
+	}
+
+	enemy_waves_ -= 1;
+
+	// Get hero spawn data
+	// Spawn enemies in a distance from the point.
+	spawn_position_ += spawn_distance_;
+
+	float half_enemy_height = 90.f;
+	if (enemy_class_)
+	{
+		AEnemyBase* default_enemy = enemy_class_->GetDefaultObject<AEnemyBase>();
+		if (default_enemy)
+		{
+			UStaticMeshComponent* static_mesh_component = default_enemy->FindComponentByClass<UStaticMeshComponent>();
+			if (static_mesh_component && static_mesh_component->GetStaticMesh())
+			{
+				half_enemy_height = static_mesh_component->GetStaticMesh()->GetBoundingBox().GetExtent().Z;
+			}
+			else 
+			{
+				USkeletalMeshComponent* skeletal_mesh_component = default_enemy->FindComponentByClass<USkeletalMeshComponent>();
+				if (skeletal_mesh_component && skeletal_mesh_component->GetSkeletalMeshAsset())
+				{
+					half_enemy_height = skeletal_mesh_component->GetSkeletalMeshAsset()->GetBounds().BoxExtent.Z;
+				}
+			}
+		}
+	}
+
+	for (int32 i = 0; i < enemy_num_; i++)
+	{
+		FVector spawn_offset = FVector(0.f, (enemy_spacing_ * (enemy_num_ - 1) / -2.f) + (i * enemy_spacing_), half_enemy_height);
+		// It may return nullptr if enemy_spacing_ is too narrow.
+		AEnemyBase* enemy = GetWorld()->SpawnActor<AEnemyBase>(enemy_class_, spawn_position_ + spawn_offset, FRotator());
+		if (enemy)
+		{
+			enemy->SpawnDefaultController();
+
+			enemies_.Add(enemy);
+		}
+	}
+}
+
+const TArray<TObjectPtr<AEnemyBase>>& UEnemySpawnerManager::GetEnemies()
+{
+	return enemies_;
+}
+
+void UEnemySpawnerManager::RemoveEnemy(AEnemyBase* enemy_defeated)
+{
+	enemies_.Remove(enemy_defeated);
+	if (enemies_.IsEmpty())
+	{
+		SpawnEnemies();
+	}
+}
+
+bool UEnemySpawnerManager::IsEnemyAllDefeated()
+{
+	return enemies_.Num() <= 0 && enemy_waves_ <= 0;
+}
