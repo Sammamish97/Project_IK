becac0c7f0e80251196ebfb3cdf25170be53f98f
Sinil Kang <rtd99062@gmail.com>
2025-03-18 00:32:45 +0900

    #140 - Camera things not yet solved.
    IKPlayerCameraManager - Currently it calculates with aspect ratio and calculate distance in both horizontal and vertical.
    
    @@ TODO:
    1. get bounding box using actors bounding box instead of actor's location.
        FBox BoundingBox(ForceInit);
        for (AActor* Actor : ActorsToInclude)
        {
            if (Actor)
            {
                BoundingBox += Actor->GetComponentsBoundingBox(true); // Include child components
            }
        }
    
    2. Aligned it to left. Detail description not yet provided.
    
    3. Make it compatible with random view vectors.

--- Source/Project_IK/Private/WorldSettings/IKPlayerCameraManager.cpp
+++ Source/Project_IK/Private/WorldSettings/IKPlayerCameraManager.cpp
@@ -31,18 +31,64 @@ void AIKPlayerCameraManager::UpdateViewTarget(FTViewTarget& OutVT, float DeltaTi
 
 	FVector center = bounding_box.GetCenter();
 	FVector extents = bounding_box.GetExtent();
+
+	// Since OutVT.POV.AspectRatio is fixed number in init stages, manually calculated it in everyframes.
+	float aspect_ratio = 1.7f;
+	if (GEngine && GEngine->GameViewport)
+	{
+		FIntPoint viewport_size = GEngine->GameViewport->Viewport->GetSizeXY();
+		if (viewport_size.Y > 0)
+		{
+			aspect_ratio = (float)viewport_size.X / (float)viewport_size.Y;
+		}
+	}
 	
 	// Compute the distance needed to fit the bounding box within the camera frustum
-	// @@ TODO: extents.Size is radius of the box, while Tan(FOV/2) is height or width. It is not a valid logic.
-	float required_distance = (extents.Size() / FMath::Tan(FMath::DegreesToRadians(OutVT.POV.FOV * 0.5f)));
-	// New Camera Position - Move backwards along the view direction
-	FVector camera_location = center - (normalize_view_vector * required_distance);
+	// OutVT.POV.FOV is vertical field of view
+	const float half_height = FMath::Tan(FMath::DegreesToRadians(OutVT.POV.FOV * 0.5f));
+	// Since extents is also half size of bounding box -> Half / Half
+	const float required_vertical_distance = (extents.Y / half_height);
+	const float required_horizontal_distance = extents.X / (half_height * aspect_ratio);
+	// The extents.X & extents.Y are chosen by current fixed camera view vector (-1, 0.f, -100.f)
+	FVector camera_location = center - (normalize_view_vector * FMath::Max(required_horizontal_distance, required_vertical_distance));
 
-	// 1. How to get width in viewport.
-	// 2. Can I get a viewfrustum geometry?
-	// Adjust left alignment - Move the camera left so actors appear left-aligned.
-	FVector left_offset = normalize_view_vector.Rotation().Quaternion().GetRightVector() * extents.X;
-	camera_location += left_offset;
+	TArray<FVector> box_corners;
+	for (int32 i = 0; i < 8; ++i)
+	{
+		FVector corner = FVector(
+			center.X + ((i & 1) ? extents.X : -extents.X),
+			center.Y + ((i & 2) ? extents.Y : -extents.Y),
+			center.Z + ((i & 4) ? extents.Z : -extents.Z)
+		);
+		box_corners.Add(corner);
+	}
+
+	APlayerController* pc = GetOwningPlayerController();
+	TArray<FVector2D> screen_points;
+	for (const FVector& corner : box_corners)
+	{
+		FVector2D screen_pos;
+		// the third parameter preferred to be true when return a window or with letterbox,
+		// default set by false for the full render target size.
+		if (pc->ProjectWorldLocationToScreen(corner, screen_pos, true))
+		{
+			screen_points.Add(screen_pos);
+		}
+	}
+
+	float left_most_x = FLT_MAX;
+	for (const FVector2D& pos : screen_points)
+	{
+		left_most_x = FMath::Min(left_most_x, pos.X);
+	}
+
+
+
+	//// 1. How to get width in viewport.
+	//// 2. Can I get a viewfrustum geometry?
+	//// Adjust left alignment - Move the camera left so actors appear left-aligned.
+	//FVector left_offset = normalize_view_vector.Rotation().Quaternion().GetRightVector() * extents.X;
+	//camera_location += left_offset;
 
 	OutVT.POV.Location = camera_location;// FMath::VInterpTo(GetCameraLocation(), camera_location, DeltaTime, 2.f);
 	OutVT.POV.Rotation = normalize_view_vector.Rotation();
