b35590f643ee5b77416e9a9efe39eba09c69e0fa
Sinil Kang <rtd99062@gmail.com>
2025-03-23 23:46:38 +0900

    #143 - Working on camera left aligns.
    IKPlayerCameraManager - working on it.

--- Source/Project_IK/Private/WorldSettings/IKPlayerCameraManager.cpp
+++ Source/Project_IK/Private/WorldSettings/IKPlayerCameraManager.cpp
@@ -27,7 +27,7 @@ void AIKPlayerCameraManager::UpdateViewTarget(FTViewTarget& OutVT, float DeltaTi
 	FVector center = bounding_box.GetCenter();
 	FVector extents = bounding_box.GetExtent();
 
-	const FRotator camera_view_rotator = normalize_view_vector.Rotation(); 
+	const FRotator camera_view_rotator = normalize_view_vector.Rotation();
 	const FQuat camera_view_quaternion = camera_view_rotator.Quaternion();
 
 	// Since OutVT.POV.AspectRatio is fixed number in init stages, manually calculated it in everyframes.
@@ -43,22 +43,44 @@ void AIKPlayerCameraManager::UpdateViewTarget(FTViewTarget& OutVT, float DeltaTi
 	const float required_up_distance = FMath::Abs((FVector::DotProduct(up_vector, extents) * aspect_ratio) / tan);
 	const float required_right_distance = FMath::Abs(FVector::DotProduct(right_vector, extents) / tan);
 	const float required_forward_distance = FMath::Abs(FVector::DotProduct(forward_vector, extents));
-	const float camera_distance = (FMath::Max(required_up_distance, required_right_distance) + required_forward_distance) * zoom_padding_;
+	const float camera_distance = (FMath::Max(required_up_distance, FMath::Abs(required_right_distance)) + required_forward_distance) * zoom_padding_;
 	FVector camera_location = center - (normalize_view_vector * camera_distance);
 
 
-	DrawDebugDirectionalArrow(GetWorld(), center, center + forward_vector * required_forward_distance, 10.f, FColor::Red);
-	DrawDebugDirectionalArrow(GetWorld(), center, center + right_vector * required_right_distance, 10.f, FColor::Green);
-	DrawDebugDirectionalArrow(GetWorld(), center, center + up_vector * required_up_distance, 10.f, FColor::Blue);
+	DrawDebugDirectionalArrow(GetWorld(), center, center + forward_vector * required_forward_distance, 10.f, FColor::Cyan);
+	DrawDebugDirectionalArrow(GetWorld(), center, center + right_vector * required_right_distance, 10.f, FColor::Magenta);
+	DrawDebugDirectionalArrow(GetWorld(), center, center + up_vector * required_up_distance, 10.f, FColor::Yellow);
+	
+	
+	float most_negative_Y = MAX_FLT;
+	FVector corner_bounding_box_camera_space;
+	for (int32 i = 0; i < 8; i++)
+	{
+		// Build the corner in local space
+		FVector corner = FVector(
+			(i & 1) ? -extents.X : extents.X,
+			(i & 2) ? -extents.Y : extents.Y,
+			(i & 4) ? -extents.Z : extents.Z
+		);
 
-	const FVector left_middle_bounding_box_camera_space = center - required_forward_distance * forward_vector - required_right_distance * right_vector;
+		// Transform into camera space
+		FVector camera_space_corner = camera_view_quaternion.UnrotateVector(corner);
+		DrawDebugDirectionalArrow(GetWorld(), center, center + corner, 10.f, FColor::Emerald);
+
+		if (camera_space_corner.Y < most_negative_Y)
+		{
+			most_negative_Y = camera_space_corner.Y;
+			corner_bounding_box_camera_space = corner;
+		}
+	}
+	corner_bounding_box_camera_space += center;
 
 	APlayerController* pc = GetOwningPlayerController();
 
 	// Adjust left alignment - Move the camera left so actors appear left-aligned.
 	FVector pos = FVector::ZeroVector;
 	FVector vec = FVector::ZeroVector;
-	FVector left_edged_point = FVector::ZeroVector;
+	FVector left_edge_point = FVector::ZeroVector;
 	FIntPoint viewport_size = GEngine->GameViewport->Viewport->GetSizeXY();
 	// Get left edge point.
 	if (pc->DeprojectScreenPositionToWorld(0.f, viewport_size.Y / 2.f, pos, vec))
@@ -69,15 +91,16 @@ void AIKPlayerCameraManager::UpdateViewTarget(FTViewTarget& OutVT, float DeltaTi
 			UE_LOG(LogTemp, Error, TEXT("Divided by zero in AIKPlayerCameraManager::UpdateViewTarget"));
 		}
 		// The old formula was it: (center.Z - camera_location.Z) / vec.Z;
-		const float t = FVector::DotProduct(camera_view_vector_, (left_middle_bounding_box_camera_space - camera_location)) / denominator; 
-		left_edged_point = camera_location + vec * t;
+		const float t = FVector::DotProduct(camera_view_vector_, (corner_bounding_box_camera_space - camera_location)) / denominator;
+		left_edge_point = camera_location + vec * t;
 	}
+	DrawDebugDirectionalArrow(GetWorld(), corner_bounding_box_camera_space, left_edge_point, 10.f, FColor::Yellow);
 	// It was an old formula to calculate left offset
 	// (-normalize_view_vector.Rotation().Quaternion().GetRightVector())* (left_edged_point.Y - (center.Y - extents.Y) + left_edge_padding_);
-	FVector left_offset = left_middle_bounding_box_camera_space - left_edged_point + (-normalize_view_vector.Rotation().Quaternion().GetRightVector()) * (left_edge_padding_);
+	FVector left_offset = corner_bounding_box_camera_space - left_edge_point + (-normalize_view_vector.Rotation().Quaternion().GetRightVector()) * (left_edge_padding_);
 
-	camera_location += left_offset;
-	
+	// camera_location += left_offset;
+
 
 	OutVT.POV.Location = camera_location + camera_location_offset_;// FMath::VInterpTo(GetCameraLocation(), camera_location, DeltaTime, 2.f);
 	OutVT.POV.Rotation = camera_view_rotator;
