14583515e977eed9ab9ceced13d207e5fef3ea9b
chunmook kim <chunmook.kim97@gmail.com>
2025-04-24 11:16:16 +0900

    #181 - 지진(Quake) 세트 효과 구현
    지진(Quake)의 2/3/6세트 효과를 간단히 구현하였다. 6세트 효과(세라핀 패시브)는 구조를 크게 refactoring을 해야하는 필요성이 있어, 일단은 더 간단하지만 본질은 비슷한 효과, 15초에 한번 스킬 가속 80 증가로 변경하였다.

--- Source/Project_IK/Private/Managers/SetBonusManager.cpp
+++ Source/Project_IK/Private/Managers/SetBonusManager.cpp
@@ -12,6 +12,7 @@ See LICENSE file in the project root for full license information.
 #include "Abilities/SetBonuses/SetBonus_Chariot.h"
 #include "Abilities/SetBonuses/SetBonus_Dagger.h"
 #include "Abilities/SetBonuses/SetBonus_GreatBow.h"
+#include "Abilities/SetBonuses/SetBonus_Quake.h"
 #include "Abilities/SetBonuses/SetBonus_Tempest.h"
 #include "Abilities/SetBonuses/SetBonus_Viper.h"
 
@@ -33,13 +34,13 @@ TObjectPtr<USetBonusBase> USetBonusManager::GetSetBonus(ERuneSetType type)
 
 	case ERuneSetType::Tempest:
 		return NewObject<USetBonus_Tempest>(this, tempest_);
+
+	case ERuneSetType::Quake:
+		return NewObject<USetBonus_Quake>(this, quake_);
 		
 	// case ERuneSetType::Poet:
 	// 	return NewObject<USetBonus_Poet>(this, poet_);
 	//
-		
-	// case ERuneSetType::Quake:
-	// 	return NewObject<USetBonus_Quake>(this, quake_);
 	}
 	return nullptr;
 }
