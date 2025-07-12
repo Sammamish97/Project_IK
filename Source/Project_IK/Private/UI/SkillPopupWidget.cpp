eda95d9c20d62d2ce20e0f59ab512c0b3703669e
ChunMook Kim <105954410+ChunMook-Kim@users.noreply.github.com>
2025-07-12 22:33:12 +0900

    #226 - 놓친 코드 삽입.
    놓친 변경점을 추가하였다.

--- Source/Project_IK/Private/UI/SkillPopupWidget.cpp
+++ Source/Project_IK/Private/UI/SkillPopupWidget.cpp
@@ -22,5 +22,6 @@ void USkillPopupWidget::UpdatePopupData(const FItemData& item_data)
 {
 	thumbnail_->SetBrushFromTexture(item_data.thumbnail);
 	skill_name_->SetText(item_data.name_);
-	skill_detail_->SetText(item_data.detail_);
+	//IKTODO: 이후 적절한 값을 Evaluate에 넣어야 한다.
+	//skill_detail_->SetText(item_data.detail_.Evaluate());
 }
