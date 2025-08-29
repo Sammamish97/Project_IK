7c21b5e0b102c78091b4590016e1fc750a5e4122
Sinil Kang <rtd99062@gmail.com>
2025-08-30 00:11:16 +0900

    #194 - Apply the Magnetized particle.
    BP_AT_MagnetizedBullet - Set reference to NS_MagnetizedInvoked.
    
    AT_MagnetizedBullet - Spawn the niagara system properly.

--- Source/Project_IK/Private/Abilities/ActiveSkills/AT_MagnetizedBullet.cpp
+++ Source/Project_IK/Private/Abilities/ActiveSkills/AT_MagnetizedBullet.cpp
@@ -16,6 +16,9 @@ See LICENSE file in the project root for full license information.
 #include "Components/AudioComponent.h"
 #include "Abilities/Buffs/BF_MagnetizedBullet.h"
 
+#include "NiagaraFunctionLibrary.h"
+#include "NiagaraComponent.h"
+
 UAT_MagnetizedBullet::UAT_MagnetizedBullet()
 {
 	target_param_ = FTargetParameters(ETargetingMode::Actor, ETargetType::Allies, 0.f, 0.f, true);
@@ -29,6 +32,18 @@ void UAT_MagnetizedBullet::InitActiveSkill(AActor* skill_owner, const FActiveSki
 }
 
 bool UAT_MagnetizedBullet::ActivateSkill(const FTargetResult& TargetResult)
+{
+	AHeroBase* hero = Cast<AHeroBase>(skill_owner_);
+	if (hero)
+	{
+		buff_->ApplyBuff(hero);
+
+		ApplyFXs(hero);
+	}
+	return Super::ActivateSkill(TargetResult);
+}
+
+void UAT_MagnetizedBullet::ApplyFXs(AHeroBase* hero)
 {
 	UAudioComponent* audio_component = nullptr;
 	if (skill_owner_)
@@ -52,11 +67,16 @@ bool UAT_MagnetizedBullet::ActivateSkill(const FTargetResult& TargetResult)
 		audio_component->FadeOut(duration, 0.2f);
 	}
 
-	AHeroBase* hero = Cast<AHeroBase>(skill_owner_);
-	if (hero)
+	hero->ChangeGunShotSoundTemporariliy(EAudioType::MagnetizedGunShot, duration);
+
+
+	if (magnetized_invoked_particle_.IsValid() == false)
 	{
-		buff_->ApplyBuff(hero);
-		hero->ChangeGunShotSoundTemporariliy(EAudioType::MagnetizedGunShot, duration);
+		magnetized_invoked_particle_.LoadSynchronous();
 	}
-	return Super::ActivateSkill(TargetResult);
+	UNiagaraComponent* niagara_component = UNiagaraFunctionLibrary::SpawnSystemAttached(magnetized_invoked_particle_.Get(), skill_owner_->GetRootComponent(), FName(""), FVector::ZeroVector, FRotator::ZeroRotator, EAttachLocation::KeepRelativeOffset, true);
+	if (niagara_component)
+	{
+		niagara_component->SetVariableFloat(FName("duration"), duration);
+	}
 }
