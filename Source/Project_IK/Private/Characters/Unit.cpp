27784c486843214703b8b233af32a861bd827417
chunmook kim <chunmook.kim97@gmail.com>
2025-09-09 15:31:04 +0900

    #261 - 무기/영웅/적 스테이터스 추가.
    무기/영웅/적을 위한 스테이터스를 추가하였다.

--- Source/Project_IK/Private/Characters/Unit.cpp
+++ Source/Project_IK/Private/Characters/Unit.cpp
@@ -180,18 +180,19 @@ bool AUnit::IsDead() const
 
 void AUnit::SetDamageUI(FDamageData data, bool is_evaded)
 {
-	UNiagaraComponent* damage_ui = UNiagaraFunctionLibrary::SpawnSystemAtLocation(this, damage_ui_system_, hp_widget_component_->GetComponentLocation());
-
-	damage_ui->SetBoolParameter(FName("IsMissed"), is_evaded);
-	damage_ui->SetFloatParameter(FName("DamageAmount"), data.atk_base_dmg_ + data.skill_power_base_dmg_);
-	if (data.is_critical_shot_)
+	if (UNiagaraComponent* damage_ui = UNiagaraFunctionLibrary::SpawnSystemAtLocation(this, damage_ui_system_, hp_widget_component_->GetComponentLocation()))
 	{
-		damage_ui->SetColorParameter(FName("Color"), FLinearColor::Red / 5.f);
-		damage_ui->SetFloatParameter(FName("SizeMultiplier"), 3.f);
-	}
-	else
-	{
-		damage_ui->SetColorParameter(FName("Color"), FLinearColor::Blue / 5.f);
+		damage_ui->SetBoolParameter(FName("IsMissed"), is_evaded);
+		damage_ui->SetFloatParameter(FName("DamageAmount"), data.atk_base_dmg_ + data.skill_power_base_dmg_);
+		if (data.is_critical_shot_)
+		{
+			damage_ui->SetColorParameter(FName("Color"), FLinearColor::Red / 5.f);
+			damage_ui->SetFloatParameter(FName("SizeMultiplier"), 3.f);
+		}
+		else
+		{
+			damage_ui->SetColorParameter(FName("Color"), FLinearColor::Blue / 5.f);
+		}
 	}
 }
 
