593a24ac91d8834939437176cec055c9f5799292
ChunMook Kim <105954410+ChunMook-Kim@users.noreply.github.com>
2025-09-27 15:03:31 +0900

    #278 - 테스트용 코드 제거

--- Source/Project_IK/Private/Subsystems/LevelTransitionSubsystem.cpp
+++ Source/Project_IK/Private/Subsystems/LevelTransitionSubsystem.cpp
@@ -35,9 +35,6 @@ void ULevelTransitionSubsystem::UpdateSpawnData(const TMap<EHeroType, FSpawnData
 {
 	spawn_data_.Empty();
 	spawn_data_ = data;
-	//
-	spawn_data_[EHeroType::Hero1].is_dead_ = true;
-	//
 }
 
 void ULevelTransitionSubsystem::UpdateSpawnDataIdx(EHeroType type, FSpawnData data)
