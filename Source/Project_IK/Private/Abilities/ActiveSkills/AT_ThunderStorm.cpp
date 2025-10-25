901860577903301d7ac865b36e1af67c86ed4178
Sinil Kang <rtd99062@gmail.com>
2025-10-25 14:31:39 +0900

    #179, #264 - Modify thunder num by rarity. Debug duplicated sounds on setting.
    SC_UI - Add sound concurrency for UI.
    
    AT_ThunderStorm - Modify thunder num by rarity. 4 times for B. 6 times for A.
    
    SettingWidget - Play sound on codes not Blueprint.

--- Source/Project_IK/Private/Abilities/ActiveSkills/AT_ThunderStorm.cpp
+++ Source/Project_IK/Private/Abilities/ActiveSkills/AT_ThunderStorm.cpp
@@ -24,6 +24,19 @@ UAT_ThunderStorm::UAT_ThunderStorm()
 	ai_holding_time_ = 1.5f;
 }
 
+void UAT_ThunderStorm::InitActiveSkill(AActor* skill_owner, const FActiveSkillData& skill_data)
+{
+	Super::InitActiveSkill(skill_owner, skill_data);
+	if (IsUpgradedActiveSkill(skill_data_.type_))
+	{
+		max_storm_count_ = 6;
+	}
+	else
+	{
+		max_storm_count_ = 4;
+	}
+}
+
 bool UAT_ThunderStorm::CanActivateSkill(const FTargetResult& TargetResult)
 {
 	return visual_actor_class_ != nullptr;
@@ -37,7 +50,7 @@ bool UAT_ThunderStorm::ActivateSkill(const FTargetResult& TargetResult)
 	visual_actor_ = skill_owner_->GetWorld()->SpawnActor<AThunderStorm>(visual_actor_class_, TargetResult.target_location_, FRotator::ZeroRotator, spawn_params);
 	if (visual_actor_)
 	{
-		visual_actor_->SetNecessaryData(target_param_.radius_, skill_data_.skill_power_scales_[0], damage_, skill_owner_);
+		visual_actor_->SetNecessaryData(target_param_.radius_, skill_data_.skill_power_scales_[0], damage_, skill_owner_, max_storm_count_);
 		return Super::ActivateSkill(TargetResult);
 	}
 	return false;
